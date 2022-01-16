83bdf5e8874582fd6dfc48c624cfde45ec54f809
kdarlishta <krenar@rentprofile.co>
2022-01-17 00:23:26 +0100

    Changed

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const withCss = require('@zeit/next-css');
+
+module.exports = {
+    images: {
+      domains: ['assets.acme.com'],
+    },
+}
+module.exports = withCss({
+    exportPathMap: function() {
+        return {
+            '/': { page: '/' },
+        };
+    },
+});
